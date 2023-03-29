eb084e3b2a9a4eb4a29d5461ea4cc5e462e0a769
Enes Doğan <enesdogansanli@gmail.com>
2023-03-29 16:26:29 +0300

    Solved shown photo problem

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from PIL import ImageTk, Image
 import matplotlib.pyplot as plt
 
 my_app = Tk()
@@ -8,6 +7,9 @@ my_app.geometry("1300x800")
 
 number_to_alphabet={0:"a", 1:"b", 2:"c", 3:"ç", 4:"d", 5:"e", 6:"f", 7:"g", 8:"ğ", 9:"h", 10:"ı",11:"i", 12:"j", 13:"k", 14:"l", 15:"m", 16:"n", 17:"o", 18:"ö", 19:"p", 20:"r",21:"s", 22:"ş", 23:"t", 24:"u", 25:"ü", 26:"v", 27:"y", 28:"z",29:"q",30:"w",31:"x"}
 alphabet_to_number={"a":0, "b":1, "c":2, "ç":3, "d":4, "e":5, "f":6, "g":7, "ğ":8, "h":9, "ı":10, "i":11, "j":12, "k":13, "l":14, "m":15, "n":16, "o":17, "ö":18, "p":19, "r":20, "s":21, "ş":22, "t":23, "u":24, "ü":25, "v":26, "y":27, "z":28, "q":29, "w":30, "x":31 }
+
+encryption_text_sorted_counter = []
+reference_text_sorted_counter = []
 
 def encryption():
     result = ""
@@ -28,64 +30,77 @@ def encryption():
     entry_3.insert(0,result)
 
 def analysis_letter_for_encryption_text():
+    global encryption_text_sorted_counter
     my_text = entry_3.get()
     my_text = my_text.lower()
     counter = {}
     for letter in my_text:
         counter[letter] = counter.get(letter, 0) + 1
     
-    sourted_counter = sorted(counter.items(), key=lambda x:x[1],reverse=True)
-    sourted_counter = dict(sourted_counter)
+    encryption_text_sorted_counter = sorted(counter.items(), key=lambda x:x[1],reverse=True)
+    encryption_text_sorted_counter = dict(encryption_text_sorted_counter)
 
-    if " " in sourted_counter:
-        del sourted_counter[" "]
+    if " " in encryption_text_sorted_counter:
+        del encryption_text_sorted_counter[" "]
 
-    names = list(sourted_counter.keys())
-    values = list(sourted_counter.values())
-    plt.bar(range(len(sourted_counter)), values, tick_label=names)
-    plt.savefig('Cipher_text_letter_analysis.png',dpi= 60)
+    names = list(encryption_text_sorted_counter.keys())
+    values = list(encryption_text_sorted_counter.values())
+    plt.bar(range(len(encryption_text_sorted_counter)), values, tick_label=names)
+    plt.savefig('Cipher_text_letter_analysis.png',dpi= 70)
 
-    # TODO: Bu iki kısmı ayırmak gerekebilir. Performans açısından daha iyi olabilir.
+    my_image1 = PhotoImage(file="Cipher_text_letter_analysis.png")
+    my_label1 = Button(my_app,image=my_image1)
+    my_label1.image = my_image1
+    my_label1.grid(row=0,column=3,sticky=EW)
 
-    my_image1 = ImageTk.PhotoImage(Image.open("Cipher_text_letter_analysis.png"))
-    my_label1 = Label(image=my_image1)
-    my_label1.grid(row=0,column=3)
-
-    return sourted_counter
+    # return encryption_text_sorted_counter
 
 def analysis_letter_for_reference_text():
+    global reference_text_sorted_counter
     my_text = entry_4.get()
     my_text = my_text.lower()
     counter = {}
     for letter in my_text:
         counter[letter] = counter.get(letter, 0) + 1
     
-    sourted_counter = sorted(counter.items(), key=lambda x:x[1],reverse=True)
-    sourted_counter = dict(sourted_counter)
+    reference_text_sorted_counter = sorted(counter.items(), key=lambda x:x[1],reverse=True)
+    reference_text_sorted_counter = dict(reference_text_sorted_counter)
 
-    if " " in sourted_counter:
-        del sourted_counter[" "]
+    if " " in reference_text_sorted_counter:
+        del reference_text_sorted_counter[" "]
 
-    names = list(sourted_counter.keys())
-    values = list(sourted_counter.values())
-    plt.bar(range(len(sourted_counter)), values, tick_label=names)
-    plt.savefig('Reference_text_letter_analysis.png',dpi= 60)
+    names = list(reference_text_sorted_counter.keys())
+    values = list(reference_text_sorted_counter.values())
+    plt.bar(range(len(reference_text_sorted_counter)), values, tick_label=names)
+    plt.savefig('Reference_text_letter_analysis.png',dpi= 70)
 
-    # TODO: Bu iki kısmı ayırmak gerekebilir. Performans açısından daha iyi olabilir.
+    # TODO: Resimlerin aynı anda ekrande gösterilmesi üzerine çalış.
+    my_image2 = PhotoImage(file="Reference_text_letter_analysis.png")
+    my_label2 = Button(my_app,image=my_image2)
+    my_label2.image = my_image2
+    my_label2.grid(row=0,column=4,sticky=EW)
 
-    my_image2 = ImageTk.PhotoImage(Image.open("Reference_text_letter_analysis.png"))
-    my_label2 = Label(image=my_image2)
-    my_label2.grid(row=0,column=4)
+    my_image1 = PhotoImage(file="Cipher_text_letter_analysis.png")
+    my_label1 = Button(my_app,image=my_image1)
+    my_label1.image = my_image1
+    my_label1.grid(row=0,column=3,sticky=EW)
 
-    return sourted_counter
+    # return reference_text_sorted_counter
 
 def decryption_the_text():
     decryption_text = ""
     my_cipher_text = entry_3.get()
-    encrypt_text_dict = new_list1 = list(map(list,analysis_letter_for_encryption_text().items()))
-    reference_text_dict = new_list2 = list(map(list,analysis_letter_for_reference_text().items()))
-    for i in range(len(encrypt_text_dict)):
-        encrypt_text_dict[i][1] = reference_text_dict[i][0]   
+    encrypt_text_dict = new_list1 = list(map(list,encryption_text_sorted_counter.items()))
+    reference_text_dict = new_list2 = list(map(list,reference_text_sorted_counter.items()))
+
+    if len(encrypt_text_dict)<=len(reference_text_dict):
+        for i in range(len(encrypt_text_dict)):
+            encrypt_text_dict[i][1] = reference_text_dict[i][0]   
+    else:
+        for i in range(len(reference_text_dict)):
+            encrypt_text_dict[i][1] = reference_text_dict[i][0] 
+        for i in range(len(reference_text_dict),len(encrypt_text_dict)):
+            encrypt_text_dict[i][1] = "x"
 
     encrypt_text_dict = dict(encrypt_text_dict)
     
@@ -117,8 +132,6 @@ def performance_text():
     result_label = Label(my_app, text="%"+performance_result)
     result_label.grid(column=1,row=8, sticky=EW, padx=5, pady=5)
         
-
-
 
 plain_text_label = Label(my_app, text="Enter plain text: ")
 plain_text_label.grid(column=0,row=0, sticky=W, padx=5, pady=5)
